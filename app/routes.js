40b707456987a22e5ab4735b4659d3eda06b5da5
Ernest Walzel <ewalzel@gmail.com>
2018-12-01 18:53:06 +0100

    Finish aktivacia eID

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const zivnostRouter = require('./zalozenie_zivnosti_routes')
+const eidRouter = require('./aktivacia_eid_routes')
 
 // Add your routes here - above the module.exports line
 router.use('/zalozenie-zivnosti', zivnostRouter)
+router.use('/aktivacia-eid', eidRouter)
 
 module.exports = router
